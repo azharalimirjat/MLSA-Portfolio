c5493ce6d31a0dd925d400f5ec273be52c955e4d
= <=>
2024-02-05 23:29:15 +0500

    Updated portfolio, added my work.

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "JavaScript in Simple for Beginners 🎉",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Get started with JavaScript sample codes with usefull comments to learn easily.",
+    url: "https://github.com/azharalimirjat/JavaScript",
+  },
+  // {
+  //   title: "Web Development for Beginners",
+  //   description:
+  //     "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
+  //   url: "https://github.com/microsoft/web-dev-for-beginners",
+  // },
+  {
+    title: "DSA PBL Using Java",
+    description:
+      "I've used the concepts of DSA to handle the data.The PBL is based on the Crashes of a city from 2010 to 2018.",
+    url: "https://github.com/azharalimirjat/DSA_PBL",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Learn Java",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
-  },
-  {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Easy to medium questions to understand and learn Java language.",
+    url: "https://github.com/azharalimirjat/Java",
   },
 ];
 
