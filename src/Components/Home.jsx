09f6d8a4051baa8027e9270968db072a623aeeba
= <=>
2024-02-09 00:55:05 +0500

    Changed my background image and added a new image.

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/Azhar.jpg";
+import image from "../images/MyImage.jpg";
 
 const imageAltText = "I'm standing at my department carrying bag.";
 
